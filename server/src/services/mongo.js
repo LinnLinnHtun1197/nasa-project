6323090121c0346646cb7f4dbab1523a465bac47
linlintun1197@gmail.com <linlintun1197@gmail.com>
2024-01-10 23:15:25 +0800

    fixed mongo connection

--- server/src/services/mongo.js
+++ server/src/services/mongo.js
@@ -1,12 +1,13 @@
 const mongoose = require('mongoose');
 
-const MONGO_URL = 'mongodb+srv://linlintun1197:io9iz0dqkemgLmOB@cluster0.38trrqp.mongodb.net/nasa?retryWrites=true&w=majority';
-
+const MONGO_URL = 'mongodb+srv://linlintun1197:io9iz0dqkemgLmOB@cluster0.38trrqp.mongodb.net/?retryWrites=true&w=majority';
+mongoose.connect(MONGO_URL);
 mongoose.connection.once('open', () => {
     console.log('MongoDB connection ready!');
 });
 
 mongoose.connection.on('error', (err) => {
+    console.log('error mongo')
     console.error(err);
 });
 
